728242f0cc13006fe4f1130f05c6438b09df990e
Tyson Fairhurst <55992872+fairhurt@users.noreply.github.com>
2021-02-25 09:21:45 -0800

    Add files via upload

---
+++ fibbonaci.py
@@ -0,0 +1,18 @@
+def recur_fibo(x):
+    if x <= 1:
+       return x
+    else:
+       return(recur_fibo(x-1) + recur_fibo(x-2))
+
+
+def fact(num):
+    factori = 1
+    if num < 0:
+       print("Sorry, factorial does not exist for negative numbers")
+    elif num == 0:
+     print("The factorial of 0 is 1")
+    else:
+     for i in range(1,num + 1):
+       factori = factori*i
+     print("The factorial of",num,"is",factori) 
+     return factori
